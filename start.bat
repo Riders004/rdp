98856aaf75c1a738998b7382c7f45464be2b0a8b
PremiumRecharger216 <131605134+PremiumRecharger216@users.noreply.github.com>
2024-08-11 21:07:28 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By lucifer" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user lucifer Lucifer@123 /add >nul
+net user lucifer JohnCena6969 /add >nul
 net localgroup administrators lucifer /add >nul
 net user lucifer /active:yes >nul
 net user installer /delete
@@ -16,6 +16,6 @@ echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken"
 echo Username: lucifer
-echo Password: Lucifer@123
+echo Password: JohnCena6969
 echo You can login now
 ping -n 10 127.0.0.1 >nul
